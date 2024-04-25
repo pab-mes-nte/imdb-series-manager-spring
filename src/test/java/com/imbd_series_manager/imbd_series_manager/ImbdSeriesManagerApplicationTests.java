7b387b79f010cdb17a8759c37de4b0a0c47cbb11
pab-mes-nter <pablo.mesa@nter.es>
2024-04-25 17:09:29 +0200

    Initial commit with models

---
+++ src/test/java/com/imbd_series_manager/imbd_series_manager/ImbdSeriesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imbd_series_manager.imbd_series_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImbdSeriesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
