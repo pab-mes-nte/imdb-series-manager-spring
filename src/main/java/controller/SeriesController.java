f9e1b1481894b14e9d17d57789967b3c792e9280
dav-rod-nte <david.rodriguez.rojas@nter.es>
2024-05-07 09:50:03 +0200

    Service creation.

--- src/main/java/controller/SeriesController.java
+++ src/main/java/controller/SeriesController.java
@@ -1,15 +1,13 @@
 package controller;
 
-import model.entities.Series;
-import model.repositories.SeriesRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import services.SeriesService;
 
 @RestController
 @RequestMapping("/api")
@@ -17,25 +15,18 @@ public class SeriesController {
     // Logger
     private static final Logger logger = LogManager.getLogger("SeriesController");
 
-    // Repositories
-    private final SeriesRepository seriesRep;
+    // Services
+    private final SeriesService seriesService;
 
-    public SeriesController(SeriesRepository seriesRep) {
-        this.seriesRep = seriesRep;
+    @Autowired
+    public SeriesController(SeriesService seriesService) {
+        this.seriesService = seriesService;
     }
 
     @GetMapping("/series")
     public String getSeries(@RequestParam(name = "name") String name) {
         logger.info("Containing:{} ", name);
-        List<Series> series = seriesRep.findByAllAttributesContainingIgnoreCase(name);
-        StringBuilder ret = new StringBuilder();
-        int nResult = 0;
-        for (Series s : series) {
-            ret.append(s.toString()).append("\n");
-            nResult++;
-        }
-        logger.info("Total results:{} ", nResult);
 
-        return ret.toString();
+        return "Hallo";
     }
 }
--- src/main/java/services/SeriesServiceImpl.java
+++ src/main/java/services/SeriesServiceImpl.java
@@ -1,13 +1,16 @@
 package services;
 
 import model.repositories.SeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeriesServiceImpl implements SeriesService {
-    // Repositories
-    final SeriesRepository seriesRep;
 
+    // Repositories
+    private final SeriesRepository seriesRep;
+
+    @Autowired
     public SeriesServiceImpl(SeriesRepository seriesRep) {
         this.seriesRep = seriesRep;
     }
