55445be72b9c0af2b2006d241698c9e388bae5a6
dav-rod-nte <david.rodriguez.rojas@nter.es>
2024-05-07 10:35:53 +0200

    Package refactor.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,13 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EntityScan("com.example.model.entities")
-@EnableJpaRepositories(basePackages = "com.example.model.repositories")
 public class Main {
 
     public static void main(String[] args) {
